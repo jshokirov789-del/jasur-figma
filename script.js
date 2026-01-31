84e0c60fdf0ac3a4f2e583723239acd5d2f79e03
jshokirov789-del <jshokirov789@gmail.com>
2026-01-31 11:55:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let person = {};
+
+let name = prompt("Ismingizni kiriting");
+
+let ID = prompt(" Mars IT ID ni kiriting");
+
+let pin = prompt(" Mars IT paroli ni kiriting");
+
+
+
+person.ism = name;
+person.ID = ID;
+person.parol = pin;
+console.log(person);
